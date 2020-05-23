36f34eb8d9fbf9e215c6c6b5842f1d516da7a4f9
Glitch (fcc-multiplayer-game) <none>
2020-05-23 21:26:00 +0000

    🍉🐠 Updated with Glitch

---
+++ public/Collectible.mjs
@@ -0,0 +1,16 @@
+class Collectible {
+  constructor(id, score, x, y) {
+
+  }
+
+}
+
+/*
+  Note: Attempt to export this for use
+  in server.js
+*/
+try {
+  module.exports = Collectible;
+} catch(e) {}
+
+export default Collectible;
