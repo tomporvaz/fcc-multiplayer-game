4fcfc2fb6083832fc16e4884a74a37fdca661519
tomporvaz <tomporvaz@gmail.com>
2020-06-21 19:47:41 -0400

    Initialized canvas with black background.

--- public/game.mjs
+++ public/game.mjs
@@ -4,3 +4,11 @@ import Collectible from './Collectible.mjs';
 const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+
+function draw(){
+    console.log("Draw called");
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+
