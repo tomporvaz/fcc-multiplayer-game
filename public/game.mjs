3ddb1ba1aa904e46060e1fe9bcd2e6dcb83419ab
tomporvaz <tomporvaz@gmail.com>
2020-06-21 20:25:53 -0400

    Reverted to .mjs.

--- public/game.mjs
+++ public/game.mjs
@@ -1,11 +1,11 @@
 import Player from './Player.mjs';
 import Collectible from './Collectible.mjs';
 
-const socket = io();
+//const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
 
-function draw(){
+export function draw(){
     console.log("Draw called");
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
