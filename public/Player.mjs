736b5eb8b66daf91616c3d646ca5bd03da87c0b8
tomporvaz <tomporvaz@gmail.com>
2020-06-22 21:15:30 -0400

    Added directionality to game movement and animation.

--- public/Player.mjs
+++ public/Player.mjs
@@ -13,23 +13,39 @@ class Player {
     //initiate date and get Milliseconds to use a synchronizing constant
     const now = new Date();
     let ms = now.getMilliseconds();
+    console.log(ms);
     
     //check if ms is in 1 of 8 alternating ranges in order to achieve an animation at 8 chomps per second
     //draw open pacman with an arc and line
     if( ( ms >= 0 && ms < 125 ) || ( ms >= 250 && ms < 375 ) || ( ms >= 500 && ms < 625 ) || ( ms >= 750 && ms < 875 )){
       context.fillStyle = this.color;
+      context.save();
+      context.translate(this.x, this.y)
+      spin(context, this.direction)
+
       context.beginPath();
-      context.arc(this.x, this.y, 14, Math.PI / 4 , Math.PI * 7 / 4, false);
-      context.lineTo( this.x - 4, this.y );
+      context.arc(0, 0, 14, Math.PI / 4 , Math.PI * 7 / 4, false);
+      context.lineTo( 0 - 4, 0 );
       context.fill();
+
+      unspin(context, this.direction);
+      context.translate( -this.x, -this.y );
+      context.restore();
     }
     //draw an almost closed circle so pacman does leave a gohst shadow in animation (drwaing full circle left a gohst in animation).
     else if (( ms >= 125 && ms < 250 ) || ( ms >= 375 && ms < 500 ) || ( ms >= 625 && ms < 750 ) || ( ms >= 875 && ms < 1000 )) {
       context.fillStyle = this.color;
+      context.translate(this.x, this.y)
+      spin(context, this.direction)
+
       context.beginPath();
-      context.arc(this.x, this.y, 14, Math.PI / 12 , Math.PI * 23 / 12, false);
-      context.lineTo( this.x - 4, this.y );
+      context.arc(0, 0, 14, Math.PI / 12 , Math.PI * 23 / 12, false);
+      context.lineTo( 0 - 4, 0 );
       context.fill();
+      
+      unspin(context, this.direction);
+      context.translate( -this.x, -this.y );
+      context.restore();
     }
   }
   
@@ -37,20 +53,24 @@ class Player {
     switch(dir){
       case "UP":
         console.log("Move player up");
+        this.direction = "UP";
         this.y -= speed;
         break;
       case "DOWN":
         console.log("Move player down");
+        this.direction = "DOWN";
         this.y += speed;
         break;
       case "LEFT":
-      console.log("Move player left");
-      this.x -= speed;
-      break;
+        console.log("Move player left");
+        this.direction = "LEFT";
+        this.x -= speed;
+        break;
       case "RIGHT":
-      console.log("Move player right");
-      this.x += speed;
-      break;
+        console.log("Move player right");
+        this.direction = "RIGHT";
+        this.x += speed;
+        break;
     }
     
   }
@@ -64,4 +84,43 @@ class Player {
   }
 }
 
+
 export default Player;
+
+//helper functions
+
+//spin canvas based on direction of player
+function spin(context, direction) {
+  switch(direction){
+    case "UP":
+      context.rotate(Math.PI * 3 / 2);
+      break;
+    case "DOWN":
+      context.rotate(Math.PI / 2);
+      break;
+    case "RIGHT":
+      context.rotate(0);
+      break;
+    case "LEFT":
+      context.rotate(Math.PI);
+      break;
+  }
+}
+
+//spin canvas based on direction of player
+function unspin(context, direction) {
+  switch(direction){
+    case "UP":
+      context.rotate(-Math.PI * 3 / 2);
+      break;
+    case "DOWN":
+      context.rotate(-Math.PI / 2);
+      break;
+    case "RIGHT":
+      context.rotate(0);
+      break;
+    case "LEFT":
+      context.rotate(-Math.PI);
+      break;
+  }
+}
